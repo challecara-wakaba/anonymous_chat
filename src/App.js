d006ade7a5ec65ada13344b9bc8b28ea861ce2ab
annin401 <cando85@icloud.com>
2019-09-08 20:24:17 +0900

    feat: とりあえずルーティングできるようにした

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+
 class App extends Component {
   render() {
-    return <React.Fragment></React.Fragment>;
+    return (
+      <React.Fragment>
+        {/* ルーティング */}
+        <Switch>
+          {/* 全て仮に用意したものです */}
+          {/* URLをブラウザに直打ちすると移動できます */}
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/login" render={() => <h1>Login</h1>} />
+          <Route
+            exact
+            path="/client/testChannel"
+            render={() => <h1>testChannel</h1>}
+          />
+          <Route
+            exact
+            path="/client/testChannel/testThread"
+            render={() => <h1>testThread</h1>}
+          />
+          <Route render={() => <p>ページが見つかりません</p>} />
+        </Switch>
+      </React.Fragment>
+    );
   }
 }
 
