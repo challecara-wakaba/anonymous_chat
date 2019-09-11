7db0c559c276eb2f25e2a750112516f1a295af87
annin401 <cando85@icloud.com>
2019-09-11 15:19:29 +0900

    feat: Thread.jsの作成（ただし，投稿機能は付いてない）

---
+++ src/containers/Thread.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
+import Message from '../components/Message';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
+const Thread = props => {
+  const classes = useStyles();
+  const replies = props.replies.map(item => (
+    <Message key={item.id} name={item.name} icon='' text={item.text} />
+  ));
+  return <List className={classes.root}>{replies}</List>;
+};
+
+// redux関連
+const mapStateToProps = state => {
+  return {
+    replies: state.message.replies
+  };
+};
+export default connect(mapStateToProps)(Thread);
