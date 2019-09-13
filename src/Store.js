5616616f0f94d761c3be7a740eb65a38f2d21570
annin401 <cando85@icloud.com>
2019-09-13 15:21:24 +0900

    chore: redux developer tools を使う設定を追加

--- src/Store.js
+++ src/Store.js
@@ -6,5 +6,7 @@ export default createStore(
   // combineReducerはネストしたものを返すので注意
   combineReducers({
     message: messageReducer
-  })
+  }),
+  // redux developer tools を使うための設定
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
