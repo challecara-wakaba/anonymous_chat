9da492f83ec8d8b588938ec2f3e2fd625b5e6acc
annin401 <cando85@icloud.com>
2019-09-20 08:09:14 +0900

    feat: VariabelSizeListでlistを書き直した

--- src/containers/Thread.js
+++ src/containers/Thread.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { FixedSizeList } from 'react-window';
+import { VariableSizeList } from 'react-window';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Message from '../components/Message';
@@ -16,6 +16,7 @@ const useStyles = makeStyles(theme => ({
 // リストのサイズを動的に変更するとバグるため，最初に決定しておく
 const THEMESPACING = 8; // 初期せってがtheme.spacing == 8px のため
 const LISTHEIGHT = document.documentElement.clientHeight - THEMESPACING * 8;
+const DEFAULTITEMSIZE = THEMESPACING * 12;
 
 const Thread = props => {
   const userName = 'annin'; // これはテストです
@@ -23,7 +24,17 @@ const Thread = props => {
   const classes = useStyles();
   const { replies, addMessage } = props;
 
-  const calculateItemSize = () => {};
+  const calculateItemSize = index => {
+    const text = replies[index].text;
+    let textFeedSum = 0;
+    for (let i = 0; i < text.length; ++i) {
+      if (text[i] === '\n') {
+        ++textFeedSum;
+      }
+    }
+    // 改行されるたびにtheme.spacing * 2 だけ高くなる
+    return DEFAULTITEMSIZE + (textFeedSum - 1) * THEMESPACING * 3;
+  };
 
   const passItemKey = (index, data) => {
     const item = data[index];
@@ -51,17 +62,17 @@ const Thread = props => {
 
   return (
     <React.Fragment>
-      <FixedSizeList
+      <VariableSizeList
         className={classes.list}
         height={LISTHEIGHT}
         width='100%'
-        itemSize={LISTHEIGHT / 9} ////////////お試しでハードコーティングしてる
+        itemSize={calculateItemSize}
         itemCount={replies.length}
         itemData={replies}
         itemKey={passItemKey}
       >
         {listItems}
-      </FixedSizeList>
+      </VariableSizeList>
       <ThreadFooter onDispatch={handleDispatch} />
     </React.Fragment>
   );
