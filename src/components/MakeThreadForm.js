0f3eb2ca4e659e41589b774264641686d47c87a2
cioccolato <you.miya4126@gmail.com>
2019-09-19 17:04:11 +0900

    feat: MakeThreadForm せんをわくにした

--- src/components/MakeThreadForm.js
+++ src/components/MakeThreadForm.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/input';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 import { spacing } from '@material-ui/system';
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap'
   },
-  input: {
-    margin: theme.spacing(1)
+  TextField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
   }
 }));
-export default function Inputs() {
+
+export default function OutlinedTextField() {
   const classes = useStyles();
   return (
-    <div className={classes.container}>
-      <Input
+    <form className={classes.container} noValidate autoComplete='off'>
+      <TextField
+        Required
+        id='outlined-required'
+        label='Title'
         placeholder='(例)物理 過去問 [2]-(1)'
-        className={classes.input}
-        inputProps={{
-          'aria-label': 'description'
-        }}
+        className={classes.TextField}
+        margin='normal'
+        variant='outlined'
       />
-    </div>
+    </form>
   );
 }
