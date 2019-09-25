b347b129d3fb50b1741d29df3d210a527d8ffe8c
annin401 <cando85@icloud.com>
2019-09-25 22:23:33 +0900

    feat: 静的版のThreadOverviewコンポーネントの作成

---
+++ src/components/ThreadOverview.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(1)
+  },
+  image: {
+    marginRight: 'auto',
+    marginBottom: theme.spacing(3),
+    marginLeft: 'auto',
+    maxHeight: 200
+  },
+  divider: {
+    height: 1
+  }
+}));
+
+// テスト用
+const TITLE = '過去問　2-1';
+const DETEAL = 'この問題の解き方教えてください';
+const PICURL = 'https://placehold.jp/120x600.png';
+const PICTURENAME = 'demo';
+const NOTIS = '2件の返信';
+
+const ThreadOverview = props => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root} elevation={1} square>
+      <Divider className={classes.divider} />
+      <CardActionArea>
+        <CardContent>
+          <Typography variant='h5' components='h2' gutterBottom>
+            {TITLE}
+          </Typography>
+          <Typography
+            variant='body1'
+            components='p'
+            color='textSecondary'
+            gutterBottom
+          >
+            {DETEAL}
+          </Typography>
+        </CardContent>
+        <CardMedia
+          className={classes.image}
+          component='img'
+          alt={PICTURENAME}
+          image={PICURL}
+          title={PICTURENAME}
+        />
+        <Divider />
+        <CardContent>
+          <Typography variant='body1'>{NOTIS}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <Divider className={classes.divider} />
+    </Card>
+  );
+};
+
+export default ThreadOverview;
