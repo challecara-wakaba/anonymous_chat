35397de2178ccb3e2bea6f0c58836405a7f392e4
cioccolato <you.miya4126@gmail.com>
2019-10-08 16:53:52 +0900

    refactor: MGB レイアウト調整

--- src/components/GoodButton.js
+++ src/components/GoodButton.js
@@ -3,10 +3,21 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
+const useStyles = makeStyles(theme => ({
+  ButtonCont: {
+    height: theme.spacing(3),
+    border: 'solid medium #FFB74C',
+    padding: 'unset'
+  },
+  IconCont: {
+    height: '18px'
+  }
+}));
 export default function GoodButton() {
+  const classes = useStyles();
   return (
-    <Button>
-      <ThumbUpIcon />
+    <Button className={classes.ButtonCont}>
+      <ThumbUpIcon className={classes.IconCont} />
     </Button>
   );
 }
