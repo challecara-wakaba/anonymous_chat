08b6aeaee5a8d9888281fb748954ee8c6fa2c08f
annin <annin401@gmail.com>
2019-10-10 01:04:08 +0900

    refactor: channel画面でインラインCSS で渡すようにした

--- src/containers/Channel.js
+++ src/containers/Channel.js
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 import ThreadCardList from '../components/ThreadCardList';
 import ThreadAddButton from '../components/ThreadAddButton';
 
+const listStyle = {
+  // VirtuosoはmakeStyleで高さと幅指定ができないためオブジェクトを作り
+  // propsで渡しinlineCSSで適応させる
+  marginTop: 69, // ヘッダー分+5pxくらい余白を取る
+  height: document.documentElement.clientHeight - 64, //headerとfooterの高さ分引く
+  width: '100%'
+};
+
 function Channel(props) {
   const { threads } = props;
   const { url } = props.match;
