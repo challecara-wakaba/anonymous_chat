c0062376a1afdd3e1c022bec1a33de68e73e0342
Cioccolato0013 <you.miya4126@gmail.com>
2019-10-11 17:47:21 +0900

    change:MGB ボタン色変更 幅変更

--- src/components/GoodButton.js
+++ src/components/GoodButton.js
@@ -6,16 +6,15 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = {
   ButtonCont: {
     display: 'inline-flex',
-    border: 'solid medium #FFB74C',
+    border: 'solid thin #FF4500',
     height: '24px',
+    minWidth: '48px',
     padding: 'unset',
-    marginTop: '8px',
-    '&:hover ': {
-      backgroundColor: '#FFB74C'
-    }
+    marginTop: '6px'
   },
   IconCont: {
-    height: '18px'
+    height: '18px',
+    color: '#FF4500'
   }
 };
 
@@ -23,7 +22,7 @@ const GoodButton = props => {
   const { classes } = props;
   return (
     <Button className={classes.ButtonCont}>
-      <ThumbUpIcon className={classes.IconCont} />
+      <ThumbUpIcon className={classes.IconCont} />9
     </Button>
   );
 };
