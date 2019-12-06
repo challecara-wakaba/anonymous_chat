1dd4e0c15a3d4e5feff9e5a1aa978307ef1c10f9
annin401 <annin401@gmail.com>
2019-12-07 00:37:54 +0900

    fix: Channelの背景色を全体に適応

--- src/containers/Channel.js
+++ src/containers/Channel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Link, useRouteMatch } from 'react-router-dom';
 
 import Header from '../components/Header';
@@ -8,9 +8,6 @@ import ThreadCardList from '../components/ThreadCardList';
 import ThreadAddButton from '../components/ThreadAddButton';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: theme.threadBackground
-  },
   list: {
     marginTop: '56px'
   }
@@ -19,11 +16,13 @@ const useStyles = makeStyles(theme => ({
 const LABEL = '# 英語';
 function Channel(props) {
   const { threads } = props;
+  const theme = useTheme();
   const classes = useStyles();
   const { url } = useRouteMatch();
 
   return (
-    <div className={classes.root}>
+    <div>
+      <style>{`body {background-color: ${theme.threadBackground}}`}</style>
       <Header location='channel' label={LABEL} />
       <div className={classes.list}>
         <ThreadCardList threads={threads} />
