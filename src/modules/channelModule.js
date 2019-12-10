d9172ecbd6b9503f7fbed7af2f890ae63b44aa3e
annin401 <annin401@gmail.com>
2019-12-10 19:25:07 +0900

    feat: Load_Threadアクションを作った

--- src/modules/channelModule.js
+++ src/modules/channelModule.js
@@ -1,41 +1,36 @@
 import shortid from 'shortid';
 
 // action type
+const LOAD_THREAD = 'LOAD_THREAD';
 const ADD_THREAD = 'ADD_THREAD';
 
 const initialState = {
-  threads: [
-    {
-      id: shortid.generate(),
-      timeStamp: new Date(),
-      title: 'ドリルp53 [B]-(1)',
-      details: 'この英文の訳がわかりません',
-      pictureURL: 'http://img-cdn.jg.jugem.jp/993/154735/20101224_1438937.jpg'
-    }
-  ]
+  threads: []
 };
 
 // reducer
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_THREAD:
-      const newThread = {
-        id: action.id,
-        timeStamp: action.timeStamp,
-        title: action.title,
-        details: action.details,
-        pictureURL: action.pictureURL
-      };
+    case LOAD_THREAD:
       return Object.assign({}, state, {
-        threads: [newThread, ...state.threads]
+        threads: action.threads
       });
 
+    case ADD_THREAD:
     default:
       return state;
   }
 }
 
 // Action Creators
+
+export function loadThread(threads) {
+  return {
+    type: LOAD_THREAD,
+    threads: threads
+  };
+}
+
 export function addThread(title, details, pictureURL) {
   return {
     type: ADD_THREAD,
