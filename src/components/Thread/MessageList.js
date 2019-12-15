f994a4a9c35cba9bcfe4df81103339d3adc1e308
annin401 <annin401@gmail.com>
2019-12-16 00:07:43 +0900

    refactor: MessageListを見やすくした

--- src/components/Thread/MessageList.js
+++ src/components/Thread/MessageList.js
@@ -8,75 +8,55 @@ export default function MessageList(props) {
   const { post, replies, userUid } = props;
   const { onKininaruClick, onGoodClick, onViewerOpen } = props;
 
+  const postDOM = Object.keys(post).length !== 0 && (
+    <FirstPost
+      key={post.id}
+      name={post.name}
+      details={post.details}
+      timeStamp={post.timeStamp}
+      pictureURL={post.pictureURL}
+      onViewerOpen={onViewerOpen}
+    />
+  );
+
+  const replyDOMs = replies.map((item, index) => {
+    // goodClickedUseersが無い時のため
+    let goodClickedUsers = item.goodClickedUsers ? item.goodClickedUsers : {};
+    // goodButtonが押された数を判断する処理
+    let goodCount = 0;
+    for (let i of Object.values(goodClickedUsers)) {
+      if (i === true) ++goodCount;
+    }
+    // goodButtonを押したか判断する処理
+    let isGoodClicked = false;
+    if (goodClickedUsers[userUid] === true) {
+      // 押していた時
+      isGoodClicked = true;
+    } else {
+      // 押していなかった時
+      isGoodClicked = false;
+    }
+    return (
+      <Message
+        key={item.id}
+        name=''
+        icon=''
+        text={item.text}
+        pictureURL={item.pictureURL}
+        timeStamp={item.timeStamp}
+        onViewerOpen={onViewerOpen}
+        //GoodButtonのprops
+        isGoodClicked={isGoodClicked}
+        goodCount={goodCount}
+        onGoodClick={() => onGoodClick(index)}
+      />
+    );
+  });
+
   return (
     <List>
-      {Object.keys(post).length !== 0 && (
-        <FirstPost
-          key={post.id}
-          name={post.name}
-          details={post.details}
-          timeStamp={post.timeStamp}
-          pictureURL={post.pictureURL}
-          onViewerOpen={onViewerOpen}
-        />
-      )}
-      {replies.map((item, index) => {
-        // goodClickedUseersが無い時のため
-        let goodClickedUsers = item.goodClickedUsers
-          ? item.goodClickedUsers
-          : {};
-        // goodButtonが押された数を判断する処理
-        let goodCount = 0;
-        for (let i of Object.values(goodClickedUsers)) {
-          if (i === true) ++goodCount;
-        }
-        // goodButtonを押したか判断する処理
-        let isGoodClicked = false;
-        if (goodClickedUsers[userUid] === true) {
-          // 押していた時
-          isGoodClicked = true;
-        } else {
-          // 押していなかった時
-          isGoodClicked = false;
-        }
-        // goodClickedUseersが無い時のため
-        let KininaruClickedUsers = item.KininaruClickedUsers
-          ? item.KininaruClickedUsers
-          : {};
-        // 気になるButtonが押された数を判断する処理
-        let KininaruCount = 0;
-        for (let i of Object.values(KininaruClickedUsers)) {
-          if (i === true) ++KininaruCount;
-        }
-        // 気になるButtonを押したか判断する処理
-        let isKininaruClicked = false;
-        if (KininaruClickedUsers[userUid] === true) {
-          // 押していた時
-          isKininaruClicked = true;
-        } else {
-          // 押していなかった時
-          isKininaruClicked = false;
-        }
-        return (
-          <Message
-            key={item.id}
-            name=''
-            icon=''
-            text={item.text}
-            pictureURL={item.pictureURL}
-            timeStamp={item.timeStamp}
-            onViewerOpen={onViewerOpen}
-            //GoodButtonのprops
-            isGoodClicked={isGoodClicked}
-            goodCount={goodCount}
-            onGoodClick={() => onGoodClick(index)}
-            //気になるボタンのprops
-            isKininaruClicked={isKininaruClicked}
-            KininaruCount={KininaruCount}
-            onKininaruClick={() => onKininaruClick(index)}
-          />
-        );
-      })}
+      {postDOM}
+      {replyDOMs}
     </List>
   );
 }
