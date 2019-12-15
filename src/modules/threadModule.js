6e0677ad96462783e32ec25354ec8c2818322049
annin401 <annin401@gmail.com>
2019-12-15 11:18:09 +0900

    refactor:  initialStateで仮に渡していたやつを消した

--- src/modules/threadModule.js
+++ src/modules/threadModule.js
@@ -9,14 +9,7 @@ const GOOD_BUTTON_CLICK = 'GOOD_BUTTON_CLICK';
 const KININARU_BUTTON_CLICK = 'KININARU_BUTTON_CLICK';
 
 const initialState = {
-  post: {
-    id: shortid.generate(),
-    name: 'annin',
-    timeStamp: new Date(),
-    title: '物理',
-    details: 'この問題がわかりません',
-    pictureURL: 'http://img-cdn.jg.jugem.jp/993/154735/20101224_1438937.jpg'
-  },
+  post: {},
   replies: []
 };
 
