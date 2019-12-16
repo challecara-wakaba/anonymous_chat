f351637a58e6fa0f3b465428e8143c3b1c114329
Cioccolato0013 <s58240ym@kurume.kosen-ac.jp>
2019-12-16 18:22:31 +0900

    change:CSS 水色を薄くした

--- src/Theme.js
+++ src/Theme.js
@@ -2,7 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   primary: '#FF5F58', //濃いコーラルピンク
-  secondary: '#95E8B8', //水色
+  secondary: '#B7EECE', //水色
   error: 'red',
   background: '#F7F3EF', //ベージュ
   threadBackground: '#FFDEDD', //淡いコーラルピンク
